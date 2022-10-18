22e043eae5703d0ad5bac9b5fa808b8854b0752b
OAMaameSerwaa <owusuansahharriet1991@gmail.com>
2022-10-18 06:11:49 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *s = "_putchar";	
+	
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
