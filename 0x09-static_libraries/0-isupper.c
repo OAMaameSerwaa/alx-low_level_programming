cc30758d87c56d01a0f1466f2a127bad914a4316
OAMaameSerwaa <owusuansahharriet1991@gmail.com>
2022-11-08 11:00:41 -0800

    Task 0

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,15 +1,1 @@
-#include "main.h"
 
-/**
- * _isupper - checks is a character is in uppercase
- * @c: integer variable
- * Return: 1 if true, 0 if otherwise
- */
-int _isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
-}
