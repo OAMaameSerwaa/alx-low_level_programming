71a3b55f408a24b591692dd050bdc35e483d90e6
OAMaameSerwaa <owusuansahharriet1991@gmail.com>
2022-11-09 04:13:44 -0800

    Task 0

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,6 @@
-#include <unistd.h>
 #include "main.h"
+#include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: character to print
- *  Return: 1 on sucess, -1 is returned on error
- */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
