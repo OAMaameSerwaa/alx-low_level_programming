6458aabdccba27300b3f714d8632e1d53f5bfdb1
OAMaameSerwaa <owusuansahharriet1991@gmail.com>
2022-11-24 10:50:16 -0800

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * print_list - Prints all the elements of a list_t list
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -17,8 +17,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		n++;
+		s++;
 	}
 
-	return (n);
+	return (s);
 }
